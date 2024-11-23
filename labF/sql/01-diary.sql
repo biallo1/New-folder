53e833059905facb895ff1c87c1ab0cbf9f05aed
biallo1 <143711217+biallo1@users.noreply.github.com>
2024-11-23 19:40:43 +0100

    Delete labF directory

--- labF/sql/01-diary.sql
+++
@@ -1,9 +0,0 @@
-create table diary
-(
-    id      integer not null
-        constraint post_pk
-            primary key autoincrement,
-    date text not null,
-    subject text not null,
-    content text not null
-);
